ee388f0e1934dfe52455ec4f1ed5418569440ab3
cdxker <fdenzell@gmail.com>
2025-02-03 02:10:42 -0800

    feature: updated bootstrap, added 2 new commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
 cd /home
 git clone https://github.com/cdxker/barista-user barista
-sudo useradd -d barista -s /bin/bash barista
+sudo useradd -m barista -s /bin/bash
+sudo chown -R barista:barista /home/barista
+sudo usermod -d /home/barista/ barista
